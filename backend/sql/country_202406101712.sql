40ab48550a827e857e3ca44b9113a0fef1037914
SuperRookiee <superrookiee@chunjae.co.kr>
2024-06-10 17:13:48 +0900

    add sample sql

---
+++ backend/sql/country_202406101712.sql
@@ -0,0 +1,18 @@
+INSERT INTO Animals.country (name,continent) VALUES
+	 ('대한민국','Asia'),
+	 ('일본','Asia'),
+	 ('미국','North America'),
+	 ('벨기에','Europe'),
+	 ('프랑스','Europe'),
+	 ('캐나다','North America'),
+	 ('스위스','Europe'),
+	 ('베트남','Asia'),
+	 ('독일','Europe'),
+	 ('스페인','Europe');
+INSERT INTO Animals.country (name,continent) VALUES
+	 ('중국','Asia'),
+	 ('태국','Asia'),
+	 ('이집트','Africa'),
+	 ('러시아','Europe'),
+	 ('호주','Oceania'),
+	 ('이탈리아','Europe');
